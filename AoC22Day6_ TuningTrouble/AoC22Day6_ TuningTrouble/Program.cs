f4869d9a8d7b556f48b02714a19d65402ddb92aa
DavidMWN <davidmwnovak@gmail.com>
2022-12-12 16:20:18 -0600

    Advent of Code 2022 Day 6 Solution

---
+++ AoC22Day6_ TuningTrouble/AoC22Day6_ TuningTrouble/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AoC22Day6__TuningTrouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1
+
+            string filePath = "../../../TuningInput.txt";
+
+            string input = File.ReadAllText(filePath).ToString();
+
+            List<char> datastream = new List<char>();
+
+            int packetMarker = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                datastream.Add(input[i]);
+            }
+
+            for (int i = 4; i < input.Length; i++)
+            {
+                if (FindPacketMarker(datastream))
+                {
+                    packetMarker = i;
+                    break;
+                }
+
+                datastream.RemoveAt(0);
+                datastream.Add(input[i]);
+            }
+
+            Console.WriteLine(packetMarker);
+
+            // Part 2
+
+            datastream.Clear();
+
+            int messageMarker = 0;
+
+            for (int i = 0; i < 14; i++)
+            {
+                datastream.Add(input[i]);
+            }
+
+            for (int i = 14; i < input.Length; i++)
+            {
+                if (FindMessageMarker(datastream))
+                {
+                    messageMarker = i;
+                    break;
+                }
+
+                datastream.RemoveAt(0);
+                datastream.Add(input[i]);
+            }
+
+            Console.WriteLine(messageMarker);
+        }
+
+        static bool FindPacketMarker(List<char> data)
+        {            
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = x + 1; y < 4; y++)
+                {
+                    if (data[x] == data[y])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool FindMessageMarker(List<char> data)
+        {
+            for (int x = 0; x < 13; x++)
+            {
+                for (int y = x + 1; y < 14; y++)
+                {
+                    if (data[x] == data[y])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
