96b1a2c5785bc24ded6f6b860e381af1f78640f8
DavidMWN <davidmwnovak@gmail.com>
2022-12-27 19:43:56 -0600

    Day 14 solution

---
+++ AoC22Day14_RegolithReservoir/AoC22Day14_RegolithReservoir/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AoCDa22Day14_RegolithReservoir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1
+
+            List<string> puzzleInput = new List<string>();
+
+            puzzleInput = File.ReadAllLines("../../../PuzzleRockPaths.txt").ToList();
+
+            RockScan rockScan = new RockScan();
+
+            List<int> xCoordinates = new List<int>();
+            List<int> yCoordinates = new List<int>();
+
+            int minXCoordinate = 500;
+            int maxYCoordinate = 0;
+
+            foreach (string line in puzzleInput)
+            {
+                string[] parse = line.Split(' ');
+
+                foreach (string section in parse)
+                {
+                    if (Char.IsDigit(section[0]))
+                    {
+                        string[] input = section.Split(',');
+
+                        xCoordinates.Add(Convert.ToInt32(input[0]));
+                        yCoordinates.Add(Convert.ToInt32(input[1]));
+                    }
+                }
+
+                for (int i = 0; i < xCoordinates.Count - 1; i++)
+                {
+                    rockScan.SetRockPath(xCoordinates[i], yCoordinates[i], xCoordinates[i + 1], yCoordinates[i + 1]);
+                }
+
+                xCoordinates.Sort();
+
+                if (xCoordinates[0] < minXCoordinate)
+                    minXCoordinate = xCoordinates[0];
+
+                xCoordinates.Clear();
+
+                yCoordinates.Sort();
+
+                if (yCoordinates[yCoordinates.Count - 1] > maxYCoordinate)
+                    maxYCoordinate = yCoordinates[yCoordinates.Count - 1];
+
+                yCoordinates.Clear();
+            }
+
+            rockScan.TestPrint(minXCoordinate, maxYCoordinate);
+
+            Console.WriteLine("\n\n");
+
+            int sandCount = 0;
+            bool abyssCheck = false;
+
+            while (abyssCheck == false)
+            {
+                if (rockScan.SandFallAbyss())
+                    sandCount++;
+                else
+                    abyssCheck = true;
+            }
+
+            rockScan.TestPrint(minXCoordinate, maxYCoordinate);
+
+            Console.WriteLine("\n\n" + sandCount + "\n\n");
+
+            // Part 2
+
+            RockScan rockScan2 = new RockScan();
+
+            int minXCoordinate2 = 500;
+            int maxYCoordinate2 = 0;
+
+            foreach (string line in puzzleInput)
+            {
+                string[] parse = line.Split(' ');
+
+                foreach (string section in parse)
+                {
+                    if (Char.IsDigit(section[0]))
+                    {
+                        string[] input = section.Split(',');
+
+                        xCoordinates.Add(Convert.ToInt32(input[0]));
+                        yCoordinates.Add(Convert.ToInt32(input[1]));
+                    }
+                }
+
+                for (int i = 0; i < xCoordinates.Count - 1; i++)
+                {
+                    rockScan2.SetRockPath(xCoordinates[i], yCoordinates[i], xCoordinates[i + 1], yCoordinates[i + 1]);
+                }
+
+                xCoordinates.Sort();
+
+                if (xCoordinates[0] < minXCoordinate2)
+                    minXCoordinate2 = xCoordinates[0];
+
+                xCoordinates.Clear();
+
+                yCoordinates.Sort();
+
+                if (yCoordinates[yCoordinates.Count - 1] > maxYCoordinate2)
+                    maxYCoordinate2 = yCoordinates[yCoordinates.Count - 1];
+
+                yCoordinates.Clear();
+            }
+
+            maxYCoordinate2 += 2;
+
+            rockScan2.SetRockFloor(maxYCoordinate2);
+
+            rockScan2.TestPrint(minXCoordinate2, maxYCoordinate2);
+
+            Console.WriteLine("\n\n");
+
+            int sandCount2 = 0;
+            bool blockCheck = false;
+
+            while (blockCheck == false)
+            {
+                if (rockScan2.SandFallFloor())
+                    sandCount2++;
+                else
+                {
+                    sandCount2++;
+                    blockCheck = true;
+                }
+            }
+
+            rockScan2.TestPrint(minXCoordinate2, maxYCoordinate2);
+
+            Console.WriteLine("\n\n" + sandCount2 + "\n\n");
+        }
+    }
+}
---
+++ AoC22Day14_RegolithReservoir/AoC22Day14_RegolithReservoir/RockScan.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoCDa22Day14_RegolithReservoir
+{
+    class RockScan
+    {
+        List<List<char>> grid;
+
+        private int sandStartOffset = 0;
+
+        public RockScan()
+        {
+            grid = new List<List<char>>();
+
+            grid.Add(new List<char>());
+
+            grid[0].Add('.');
+        }
+
+        public void SetRockPath(int x1, int y1, int x2, int y2)
+        {
+            int maxXIndex = Math.Max(x1, x2) + 2;
+            int maxYIndex = Math.Max(y1, y2) + 2;
+
+            if (grid.Count < maxYIndex + 1)
+            {
+                for (int y = grid.Count; y < maxYIndex + 1; y++)
+                {
+                    grid.Add(new List<char>());
+
+                    for (int x = 0; x < grid[0].Count; x++)
+                        grid[grid.Count - 1].Add('.');
+                }
+            }
+
+            if (grid[0].Count < maxXIndex + 1)
+            {
+                for (int y = 0; y < grid.Count; y++)
+                {
+                    for (int x = grid[y].Count; x < maxXIndex + 1; x++)
+                        grid[y].Add('.');
+                }                    
+            }
+
+            // Drawing a path horizontal left
+            if (x1 > x2)
+            {
+                for (int i = x1; i >= x2; i--)
+                {
+                    grid[y1][i] = '#';
+                }
+            }
+
+            // Drawing a path horizontal right
+            if (x1 < x2)
+            {
+                for (int i = x1; i <= x2; i++)
+                {
+                    grid[y1][i] = '#';
+                }
+            }
+
+            // Drawing a path vertical up
+            if (y1 > y2)
+            {
+                for (int i = y1; i >= y2; i--)
+                {
+                    grid[i][x1] = '#';
+                }
+            }
+
+            // Drawing a path vertical down
+            if (y1 < y2)
+            {
+                for (int i = y1; i <= y2; i++)
+                {
+                    grid[i][x1] = '#';
+                }
+            }
+        }
+
+        public void SetRockFloor(int maxYIndex)
+        {
+            if (grid.Count < maxYIndex + 1)
+            {
+                for (int y = grid.Count; y < maxYIndex + 1; y++)
+                {
+                    grid.Add(new List<char>());
+
+                    for (int x = 0; x < grid[0].Count; x++)
+                        grid[grid.Count - 1].Add('.');
+                }
+            }
+
+            for (int x = 0; x < grid[0].Count; x++)
+            {
+                grid[maxYIndex][x] = '#';
+            }
+        }
+
+        public void TestPrint(int minX, int MaxY)
+        {
+            for (int y = 0; y <= MaxY; y++)
+            {
+                if (sandStartOffset == 0)
+                {
+                    for (int x = minX - 2; x < grid[y].Count; x++)
+                    {
+                        Console.Write(grid[y][x]);
+                    }
+                }
+                else
+                {
+                    for (int x = 0; x < grid[y].Count; x++)
+                        Console.Write(grid[y][x]);
+                }
+
+                Console.Write("\n");
+            }
+        }
+
+        public bool SandFallAbyss()
+        {
+            int x = 500 + sandStartOffset;
+            int y = 0;
+
+            while (y < grid.Count - 1)
+            {
+                if (DownCheck(x, y))
+                {
+                    y++;
+                    continue;
+                }
+                
+                if (DownLeftCheck(x, y))
+                {
+                    y++;
+                    x--;
+                    continue;
+                }
+
+                if (DownRightCheck(x, y))
+                {
+                    y++;
+                    x++;
+                    continue;
+                }
+
+                break;
+            }
+
+            if (y + 1 == grid.Count)
+                return false;
+
+            grid[y][x] = 'O';
+
+            return true;
+        }
+
+        public bool SandFallFloor()
+        {
+            int x = 500 + sandStartOffset;
+            int y = 0;
+
+            bool blocked = false;
+
+            while (blocked == false)
+            {
+                GridExpansionCheck(x);
+
+                if (DownCheck(x, y))
+                {
+                    y++;
+                    continue;
+                }
+
+                if (DownLeftCheck(x, y))
+                {
+                    y++;
+                    x--;
+                    continue;
+                }
+
+                if (DownRightCheck(x, y))
+                {
+                    y++;
+                    x++;
+                    continue;
+                }
+
+                blocked = true;
+            }
+
+            grid[y][x] = 'O';
+
+            if (grid[0][500 + sandStartOffset] == 'O')
+                return false;
+            else
+                return true;
+        }
+
+        private bool DownCheck(int x, int y)
+        {
+            if (grid[y + 1][x] == '.')
+                return true;
+            else
+                return false;
+        }
+
+        private bool DownLeftCheck(int x, int y)
+        {
+            if (grid[y + 1][x - 1] == '.')
+                return true;
+            else
+                return false;            
+        }
+
+        private bool DownRightCheck(int x, int y)
+        {
+            if (grid[y + 1][x + 1] == '.')
+                return true;
+            else
+                return false;           
+        }
+
+        private void GridExpansionCheck(int nextX)
+        {
+            if (nextX < 0)
+            {
+                foreach (List<char> c in grid)
+                {
+                    c.Insert(0, '.');
+                }
+
+                sandStartOffset++;
+            }
+
+            if (grid[0].Count <= nextX + 1)
+            {
+                for (int y = 0; y < grid.Count; y++)
+                {
+                    if (y == grid.Count - 1)
+                        grid[y].Add('#');
+                    else
+                        grid[y].Add('.');
+                }
+            }
+        }
+    }
+}
